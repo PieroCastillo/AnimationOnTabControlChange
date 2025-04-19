caa9d61c0616500ca24033fa415a023d84553b54
YasarYY <yasaryucel.yesilbag@yahoo.com>
2025-04-19 15:51:50 +0300

    Framework updates (.net 8.0 & Avalonia 11.2.7) and related code updates.

--- AnimationOnTabControlChange/AnimateTabControl.cs
+++ AnimationOnTabControlChange/AnimateTabControl.cs
@@ -1,42 +1,37 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.Metadata;
-using Avalonia.Styling;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AnimationOnTabControlChange
 {
-    [PseudoClasses(":normal")]
-    public class AnimateTabControl : TabControl, IStyleable
-    {
-        Type IStyleable.StyleKey => typeof(TabControl);
+	[PseudoClasses(":normal")]
+	public class AnimateTabControl : TabControl
+	{
+		protected override Type StyleKeyOverride => typeof(TabControl);
 
-        public AnimateTabControl()
-        {
-            PseudoClasses.Add(":normal");
-            this.GetObservable(SelectedContentProperty).Subscribe(OnContentChanged);
-        }
+		public AnimateTabControl()
+		{
+			PseudoClasses.Add(":normal");
+			SelectionChanged += OnContentChanged;
+		}
 
-        private void OnContentChanged(object obj)
-        {
-            if (AnimateOnChange)
-            {
-                PseudoClasses.Remove(":normal");
-                PseudoClasses.Add(":normal");
-            }
-        }
+		private void OnContentChanged(object? sender, SelectionChangedEventArgs e)
+		{
+			if (AnimateOnChange)
+			{
+				PseudoClasses.Remove(":normal");
+				PseudoClasses.Add(":normal");
+			}
+		}
 
-        public bool AnimateOnChange
-        {
-            get => GetValue(AnimateOnChangeProperty);
-            set => SetValue(AnimateOnChangeProperty, value);
-        }
+		public bool AnimateOnChange
+		{
+			get => GetValue(AnimateOnChangeProperty);
+			set => SetValue(AnimateOnChangeProperty, value);
+		}
 
-        public static readonly StyledProperty<bool> AnimateOnChangeProperty =
-            AvaloniaProperty.Register<AnimateTabControl, bool>(nameof(AnimateOnChange), true);
-    }
+		public static readonly StyledProperty<bool> AnimateOnChangeProperty =
+			AvaloniaProperty.Register<AnimateTabControl, bool>(nameof(AnimateOnChange), true);
+	}
 }
